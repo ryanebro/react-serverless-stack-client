d79707d926ecfd55e6056ffcaab6dbebe20ebf4c
Ryan Ebro <ryan.ebro@yahoo.com>
2020-06-21 14:09:52 +0800

    added Login component

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,16 @@ import { Route, Switch } from "react-router-dom";
 
 import Home from "./containers/Home/Home";
 import NotFound from "./containers/NotFound/NotFound";
+import Login from "./containers/Login/Login";
 
-export default function Routes() {
+const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
       <Route component={NotFound} />
     </Switch>
   );
 }
+
+export default Routes;
