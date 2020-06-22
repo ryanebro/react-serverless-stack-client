64bc5a08cc70f0ddad99ad6b71858458bb77f30a
Ryan Ebro <ryan.ebro@yahoo.com>
2020-06-22 11:35:07 +0800

    connect sign up to aws cognito

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,14 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import NotFound from "./containers/NotFound/NotFound";
 import Login from "./containers/Login/Login";
+import SignUp from "./containers/SignUp/SignUp";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={SignUp} />
       <Route component={NotFound} />
     </Switch>
   );
