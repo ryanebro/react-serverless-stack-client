fdcf16f78db71ef86936aec8f9b52173e1fd6e3d
Ryan Ebro <ryan.ebro@yahoo.com>
2020-06-27 19:35:51 +0800

    display single note

--- src/Routes.js
+++ src/Routes.js
@@ -6,6 +6,7 @@ import NotFound from "./containers/NotFound/NotFound";
 import Login from "./containers/Login/Login";
 import SignUp from "./containers/SignUp/SignUp";
 import NewNote from "./containers/NewNote/NewNote";
+import Notes from "./containers/Notes/Notes";
 
 const Routes = () => {
   return (
@@ -14,6 +15,7 @@ const Routes = () => {
       <Route exact path="/login" component={Login} />
       <Route exact path="/signup" component={SignUp} />
       <Route exact path="/notes/new" component={NewNote} />
+      <Route exact path="/notes/:id" component={Notes} />
       <Route component={NotFound} />
     </Switch>
   );
